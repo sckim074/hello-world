9fe31a1e372ab3b45f896c966831b332263e417d
Administrator <Administrator@ksc>
2020-12-21 17:45:41 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+ 
+public class HelloWorld {
+	public static void main(String args[]){
+	System.out.println("Hello World");
+	System.out.println("Hello ");
+	System.out.println("Hello Java!!!!!");
+	System.out.println("안녕하세요!!!!");
+	System.out.println("굿모닝 ^^");
+	}
+}
